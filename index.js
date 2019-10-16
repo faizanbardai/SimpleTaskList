a83eda1e1f5a87c648ef53935b3d4dede9570644
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-16 15:26:23 +0200

    added comments and removes unnecessary spaces

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 function addNewTask(e) {
+    //create new list item and add class to it
     var newLi = document.createElement("li");
     newLi.classList.add("taskListItem");
+
+    //add text to it
     newLi.innerText = e;
+
+    //append it to the unordered list
     document.querySelector("#myTaskList").appendChild(newLi);
+
+    //clear the input text field
     document.querySelector("#newTask").value = "";
 };
-
-
 function removeLast() {
+    //check if there any list item present; if so, then delete the last element
     myTaskList.childElementCount > 0 ? myTaskList.removeChild(myTaskList.lastChild) : null;
-
 };
 function removeFirst() {
+    //check if there any list item present; if so, then delete the first element
     myTaskList.childElementCount > 0 ? myTaskList.removeChild(myTaskList.firstChild) : null;
 };
 function getTasksAsArray() {
+    //get all the list items as node list
     var allTasks = document.querySelectorAll("#myTaskList li");
+
+    //For each list item, save it's inner text value in an array
     var allTasksAsString = [];
     allTasks.forEach((e) => allTasksAsString.push(e.innerText));
     return allTasksAsString;
 };
 function changeTaskBackgroundColor() {
+    //apply the selected color to the background of unordered list
     document.querySelector("#myTaskList").style.backgroundColor = colorPicker.value;
 };
 function bubblesort() {
+    //sort the array alphabetically
     var sortedTask = getTasksAsArray().sort();
+
+    //delete all list items
     while (myTaskList.childElementCount > 0) {
-        myTaskList.removeChild(myTaskList.lastChild);
+        removeFirst();
     };
+
+    //repopulate the list items after sorting 
+    //by calling addNewTask function and passing each list item as parameter
     sortedTask.forEach((e) => addNewTask(e));
 };
