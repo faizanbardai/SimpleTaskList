8a97abd3901af42e74ee86f19b084df4faaf2613
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-16 15:01:50 +0200

    Added sort and updated addNewTask (with parameter)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function addNewTask() {
-    var newTask = document.createElement("li");
-    newTask.classList.add("taskListItem");
-    newTask.innerText = document.querySelector("#newTask").value;
-    document.querySelector("#myTaskList").appendChild(newTask);
+function addNewTask(e) {
+    var newLi = document.createElement("li");
+    newLi.classList.add("taskListItem");
+    newLi.innerText = e;
+    document.querySelector("#myTaskList").appendChild(newLi);
     document.querySelector("#newTask").value = "";
 };
+
+
 function removeLast() {
     myTaskList.childElementCount > 0 ? myTaskList.removeChild(myTaskList.lastChild) : null;
 
@@ -20,4 +22,11 @@ function getTasksAsArray() {
 };
 function changeTaskBackgroundColor() {
     document.querySelector("#myTaskList").style.backgroundColor = colorPicker.value;
+};
+function bubblesort() {
+    var sortedTask = getTasksAsArray().sort();
+    while (myTaskList.childElementCount > 0) {
+        myTaskList.removeChild(myTaskList.lastChild);
+    };
+    sortedTask.forEach((e) => addNewTask(e));
 };
