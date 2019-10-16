38017e66a727f3abef7707ef9e59cd663e3d78e8
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-16 13:51:50 +0200

    Added changeTaskBackgroundColor function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function addNewTask() {
     var newTask = document.createElement("li");
+    newTask.classList.add("taskListItem");
     newTask.innerText = document.querySelector("#newTask").value;
     document.querySelector("#myTaskList").appendChild(newTask);
     document.querySelector("#newTask").value = "";
@@ -16,4 +17,7 @@ function getTasksAsArray() {
     var allTasksAsString = [];
     allTasks.forEach((e) => allTasksAsString.push(e.innerText));
     return allTasksAsString;
+};
+function changeTaskBackgroundColor() {
+    document.querySelector("#myTaskList").style.backgroundColor = colorPicker.value;
 };
